547082431c566bc4cf92c480be66c5af3dc0ea66
Wendy Martinez <lizete.martinez@gmail.com>
2023-06-01 12:41:26 -0400

    Updates
    Canada out-of-control fires map
    source update

---
+++ Python/canada-fire-map.py
@@ -0,0 +1,31 @@
+
+import geopandas
+import pandas as pd
+import datawrappergraphics as dw
+
+
+###Locations
+
+
+map_id_locs = "cmzSY"  #id
+
+dataOC = geopandas.read_file("../clean_map_data/locations/canada_oc_100_hec/canada_oc_100_hec.geojson")
+
+
+
+dataOC["type"] = "point"
+dataOC["icon"] = "circle-sm"
+dataOC["markerColor"] = ("#c42127")
+
+
+
+#data_locs = pd.concat([dataOC,dataBH])
+
+
+
+
+map = (dw.Map(map_id_locs)
+            .data(dataOC, append="./markers/canada/canada-location-m.json")
+            .footer(source="Natural Resources Canada,", byline ="Graeme Bruce, Wendy Martinez/CBC" )      
+            .publish()
+            )
