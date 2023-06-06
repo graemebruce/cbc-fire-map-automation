b76c90ca2c19d351d8df5276027a811fbf5708d9
graemebruce <grjbruce@gmail.com>
2023-06-06 14:14:22 -0400

    Update get_data.R

--- R/get_data.R
+++ R/get_data.R
@@ -45,10 +45,10 @@ sf::st_write(canada_oc_100_hec, dsn = paste0("clean_map_data/locations/canada_oc
 
 ########################Perimeters########################
 
-perimeter_url <- "https://cwfis.cfs.nrcan.gc.ca/geoserver/public/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=public%3Am3_polygons_current&maxFeatures=5000&outputFormat=SHAPE-ZIP"
-perimeter_temp <- tempfile(fileext = ".zip")
-download.file(perimeter_url, perimeter_temp)
-unzip(perimeter_temp,exdir = "raw_map_data/fire_perims")
+#perimeter_url <- "https://cwfis.cfs.nrcan.gc.ca/geoserver/public/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=public%3Am3_polygons_current&maxFeatures=5000&outputFormat=SHAPE-ZIP"
+#perimeter_temp <- tempfile(fileext = ".zip")
+#download.file(perimeter_url, perimeter_temp)
+#unzip(perimeter_temp,exdir = "raw_map_data/fire_perims")
 
 # perimeters_dbf_url <- "https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/perimeters.dbf"
 # download.file(perimeters_dbf_url,"raw_map_data/fire_perims/perimeters.dbf")
@@ -59,14 +59,14 @@ unzip(perimeter_temp,exdir = "raw_map_data/fire_perims")
 # perimeters_shx_url <- "https://cwfis.cfs.nrcan.gc.ca/downloads/hotspots/perimeters.shx"
 # download.file(perimeters_shx_url,"raw_map_data/fire_perims/perimeters.shx")
 
-fire_perims <- read_sf("raw_map_data/fire_perims/m3_polygons_current.shp") %>%
+#fire_perims <- read_sf("raw_map_data/fire_perims/m3_polygons_current.shp") %>%
   st_transform(4326)
 
 # fire_perims_simple <- ms_simplify(fire_perims, keep = 0.1,
 #                                   keep_shapes = FALSE)
 
-fire_perims_simple <- fire_perims
-sf::st_write(fire_perims_simple, dsn = "clean_map_data/fire_perims/fire_perims_simple.geojson", layer = "clean_map_data/fire_perims/fire_perims_simple.geojson",delete_dsn = T)
+#fire_perims_simple <- fire_perims
+#sf::st_write(fire_perims_simple, dsn = "clean_map_data/fire_perims/fire_perims_simple.geojson", layer = "clean_map_data/fire_perims/fire_perims_simple.geojson",delete_dsn = T)
 ########################Smoke########################
 
 #empty the directory first so we don't collect giant shapefiles
