7658600cd5dd5184c6ad79d4dac08e35b7b13313
graemebruce <grjbruce@gmail.com>
2023-06-15 15:29:35 -0400

    Update get_locations.R

--- R/get_locations.R
+++ R/get_locations.R
@@ -3,7 +3,7 @@ library(dplyr)
 active_fires_raw <- read.csv("https://cwfis.cfs.nrcan.gc.ca/downloads/activefires/activefires.csv")
 devtools::install_github("munichrocker/DatawRappr")
 library(DatawRappr)
-api_key <- Sys.getenv("API_KEY")
+my_api_key <- Sys.getenv("API_KEY")
 Sys.setenv(TZ = "America/Toronto")
 updateTime <- format(Sys.time(), "%H:%M:%S")
 updateDate <- format(Sys.Date(), format="%B %d, %Y")
@@ -13,10 +13,15 @@ updateTime <- format(strptime(updateTime, "%H:%M:%S"), "%I:%M %p")
 canada_fires <- active_fires_raw %>%
   dplyr::filter(stage_of_control %in% c(" OC"," BH"," UC"))
 
-dw_data_to_chart(canada_fires, "D86jR")
+dw_data_to_chart(
+  canada_fires, 
+  "D86jR",
+  api_key = my_api_key)
 
 dw_edit_chart(
   "D86jR",
-  api_key = "environment",
+  api_key = my_api_key,
   annotate = paste0("Updated on ",updateDate))
-dw_publish_chart("D86jR")
+dw_publish_chart(
+  "D86jR",
+  api_key = my_api_key)
